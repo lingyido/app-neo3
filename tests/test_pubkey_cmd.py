0a291e08e71eab47c95b69bbaa30d7129f3147d2
Francois Beutin <francois.beutin@ledger.fr>
2022-12-26 14:37:45 +0100

    New test_get_public_key_confirm_refused test

--- tests/test_pubkey_cmd.py
+++ tests/test_pubkey_cmd.py
@@ -1,8 +1,11 @@
-from apps.neo_n3_cmd import Neo_n3_Command
-from utils import create_simple_nav_instructions
 from pathlib import Path
+
+from apps.neo_n3_cmd import Neo_n3_Command
+
 from ragger.navigator import NavInsID, NavIns
 from ragger.bip import calculate_public_key_and_chaincode, CurveChoice
+from ragger.backend import RaisePolicy
+from utils import create_simple_nav_instructions
 
 ROOT_SCREENSHOT_PATH = Path(__file__).parent.resolve()
 
@@ -13,8 +16,7 @@ def test_get_public_key_no_confirm(backend, firmware):
         ref_public_key, _ = calculate_public_key_and_chaincode(curve=CurveChoice.Nist256p1, path=path)
         assert pub_key.hex() == ref_public_key
 
-from time import sleep
-def test_get_public_key_confirm(backend, firmware, navigator, test_name):
+def test_get_public_key_confirm_ok(backend, firmware, navigator, test_name):
     client = Neo_n3_Command(backend)
     path = "m/44'/888'/0'/0/0"
     with client.get_public_key_async(bip44_path=path):
@@ -28,8 +30,23 @@ def test_get_public_key_confirm(backend, firmware, navigator, test_name):
                        NavIns(NavInsID.TOUCH, (197,276)),
                        NavIns(NavInsID.USE_CASE_ADDRESS_CONFIRMATION_EXIT_QR),
                        NavIns(NavInsID.USE_CASE_ADDRESS_CONFIRMATION_CONFIRM)]
-        navigator.navigate_and_compare(ROOT_SCREENSHOT_PATH, test_name, nav_ins, first_instruction_wait=2.0, middle_instruction_wait=1.0, last_instruction_wait=2.0)
+        navigator.navigate_and_compare(ROOT_SCREENSHOT_PATH, test_name, nav_ins, first_instruction_wait=4.0, middle_instruction_wait=4.0, last_instruction_wait=4.0)
     pub_key = backend.last_async_response.data
 
     ref_public_key, _ = calculate_public_key_and_chaincode(curve=CurveChoice.Nist256p1, path=path)
     assert pub_key.hex() == ref_public_key
+
+def test_get_public_key_confirm_refused(backend, firmware, navigator, test_name):
+    client = Neo_n3_Command(backend)
+    path = "m/44'/888'/0'/0/0"
+    with client.get_public_key_async(bip44_path=path):
+        if backend.firmware.device == "nanos":
+            nav_ins = create_simple_nav_instructions(3 + 1)
+        elif backend.firmware.device.startswith("nano"):
+            nav_ins = create_simple_nav_instructions(2 + 1)
+        else:
+            nav_ins = [NavIns(NavInsID.USE_CASE_ADDRESS_CONFIRMATION_CANCEL)]
+        backend.raise_policy = RaisePolicy.RAISE_NOTHING
+        navigator.navigate_and_compare(ROOT_SCREENSHOT_PATH, test_name, nav_ins, first_instruction_wait=4.0, middle_instruction_wait=4.0, last_instruction_wait=4.0)
+    rapdu = backend.last_async_response
+    assert rapdu.status == 0x6985 # Deny error
