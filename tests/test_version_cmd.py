aa13e2ac652fb11a5131e4962ec61f743bf8ca15
Francois Beutin <francois.beutin@ledger.fr>
2023-03-14 11:07:35 +0100

    Fix tests for version 0.3.0

--- tests/test_version_cmd.py
+++ tests/test_version_cmd.py
@@ -2,4 +2,4 @@ from apps.neo_n3_cmd import Neo_n3_Command
 
 def test_version(backend, firmware):
     client = Neo_n3_Command(backend)
-    assert client.get_version() == (0, 2, 0)
+    assert client.get_version() == (0, 3, 0)
