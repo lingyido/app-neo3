f2005398c042a8bd8ba078d27cb6673bd2f92ded
Francois Beutin <francois.beutin@ledger.fr>
2023-05-17 17:12:34 +0200

    Update version snapshot and add a script to recover the version from the Makefile

--- tests/test_name_version.py
+++ tests/test_name_version.py
@@ -1,9 +1,14 @@
 from apps.neo_n3_cmd import Neo_n3_Command
+from apps.get_version import get_version_from_makefile
+
+LEDGER_MAJOR_VERSION, LEDGER_MINOR_VERSION, LEDGER_PATCH_VERSION = get_version_from_makefile()
 
 def test_get_app_and_version(backend, backend_name, firmware):
     client = Neo_n3_Command(backend)
 
-    app_name, version = client.get_app_and_version()
-
+    app_name, version_str = client.get_app_and_version()
     assert app_name == "NEO N3"
-    assert version == "0.3.1"
+    version = version_str.split('.')
+    assert version[0] == str(LEDGER_MAJOR_VERSION)
+    assert version[1] == str(LEDGER_MINOR_VERSION)
+    assert version[2] == str(LEDGER_PATCH_VERSION)
